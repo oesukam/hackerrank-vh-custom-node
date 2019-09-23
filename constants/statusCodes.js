030602d05fd735881f0ddd374e11946204ecb74e
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-09-23 14:20:14 +0200

    change status code export es6 to common js from

--- constants/statusCodes.js
+++ constants/statusCodes.js
@@ -1,8 +1,8 @@
-export const OK = 200;
-export const CREATED = 201;
-export const UNAUTHORIZED = 401;
-export const NOT_FOUND = 404;
-export const FORBIDDEN = 403;
-export const BAD_REQUEST = 400;
-export const CONFLICT = 409;
-export const SERVER_ERROR = 500;
+module.exports.OK = 200;
+module.exports.CREATED = 201;
+module.exports.UNAUTHORIZED = 401;
+module.exports.NOT_FOUND = 404;
+module.exports.FORBIDDEN = 403;
+module.exports.BAD_REQUEST = 400;
+module.exports.CONFLICT = 409;
+module.exports.INTERNAL_SERVER_ERROR = 500;
